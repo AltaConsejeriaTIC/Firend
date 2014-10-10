5dde2b4275479328e33954b24d04b39ed1e0f4b7
Cegard <Cegard689@gmail.com>
2014-10-10 17:47:30 -0500

    twitter integrado

--- Analyser.py
+++ Analyser.py
@@ -3,3 +3,14 @@
 import RInteractor as ri
 
 print ri.focos
+
+kilometrosNorteSur = 50 # coordenadas verticales esquina superior derecha
+kilometrosEsteOeste = 60 # coordenadas horizonales esquina superior derecha
+areaPorPixel = 2 # cantidad de metros cuadrados contenidos en un pixel
+
+# debido a la gran cantidad de focos en la imagen solo se procesar? el primero
+# (as? no bloquearan la cuenta por spam)
+
+coordenadaX = (focos[0,0]*areaPorPixel)+kilometrosEsteOeste
+coordenadaY = (focos[0,1]*areaPorPixel)+kilometrosNorteSur
+
